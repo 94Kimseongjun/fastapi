2041f5be933a632357820e815ebb8338d77586df
sjkim <94kimseongjun@gmail.com>
2023-03-15 17:46:11 +0900

    commit

--- main.py
+++ main.py
@@ -43,22 +43,114 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
+"""
 class SearchUserData(BaseModel):
     nickName: str
+"""
 
-@app.post("/searchNickName")
-async def search_nick_name(data: SearchUserData):
+def call_nexon_api(url):
+    try:
+        response = requests.get(url, headers=header)
+        if response.status_code==200:
+            return response
+        else:
+            raise HTTPException(status_code=response.status_code, detail=response.text)
+    except requests.RequestException as error:
+        raise HTTPException(status_code=500, detail=str(error))
+
+
+
+@app.get("/searchNickName")
+async def search_nick_name(nickName: str):
+    # 최종 결과값
+    result = {
+        "accessId": "",
+        "nickname": "",
+        "level": "",
+        "rank": "",
+        "autoRank": "",
+        "matchList": ""
+    }
+
+    # nickName 으로 accessId,level 가져오기
+    fifa4_nick_name_api_url="https://api.nexon.co.kr/fifaonline4/v1.0/users?nickname={}".format(nickName)
+    nick_name_response = call_nexon_api(fifa4_nick_name_api_url)
+
+    if nick_name_response:
+        nick_name_jsonStr = nick_name_response.json()
+        accessId = nick_name_jsonStr['accessId']
+        nickname = nick_name_jsonStr['nickname']
+        level = nick_name_jsonStr['level']
+        result["accessId"] = accessId
+        result["nickname"] = nickname
+        result["level"] = level
+    else:
+        raise HTTPException(status_code=nick_name_response.status_code, detail=nick_name_response.text)
+
+    # accessId로 최고등급 가져오기
+    fifa4_maxdivision_api_url="https://api.nexon.co.kr/fifaonline4/v1.0/users/{}/maxdivision".format(accessId)
+    maxdivision_response = call_nexon_api(fifa4_maxdivision_api_url)
+    if maxdivision_response:
+        maxdivision_jsonStr = maxdivision_response.json()
+        for maxdivision_info in maxdivision_jsonStr:
+            matchType = maxdivision_info['matchType']
+            division = maxdivision_info['division']
+            achievementDate = maxdivision_info['achievementDate']
+            maxdivision_info['division'] = division_dict[division]
+            temp_time = achievementDate.split("T")
+            maxdivision_info['achievementDate'] = temp_time[0]
+            if matchType == 50:  # 공식경기
+                maxdivision_info['matchType'] = '공식경기'
+                result["rank"] = maxdivision_info
+            elif matchType == 52:  # 감독모드
+                maxdivision_info['matchType'] = '감독모드'
+                result["autoRank"] = maxdivision_info
+    else:
+        raise HTTPException(status_code=maxdivision_response.status_code, detail=maxdivision_response.text)
+
+
+    match_history_api_url = "https://api.nexon.co.kr/fifaonline4/v1.0/users/{accessid}/matches?matchtype={matchtype}&offset={offset}&limit={limit}".format(accessid=accessId,matchtype=50,offset=0,limit=20)
+    match_history_response = call_nexon_api(match_history_api_url)
+    match_list = []
+    if match_history_response:
+        match_id_list = match_history_response.json()
+        # 최근 20경기 분석 후 평균 점유율, 헤더 시도 비율, 중거리 슛 시도 비율 가져오기
+        detail_match_api_url = "https://api.nexon.co.kr/fifaonline4/v1.0/matches/{matchid}"
+        cnt = 0
+        for match_id in match_id_list:
+
+            detail_match_response = call_nexon_api(detail_match_api_url.format(matchid=match_id))
+            if detail_match_response:
+                detail_match = detail_match_response.json()
+                match_list.append(detail_match)
+                #matchId = detail_match["matchId"]
+                #matchDate = detail_match["matchDate"]
+                #matchInfo = detail_match["matchInfo"]
+            cnt += 1
+            if cnt == 10:
+                break
+        result["matchList"] = match_list
+    else:
+        raise HTTPException(status_code=match_history_response.status_code, detail=match_history_response.text)
+    return result
+
+
+"""  
+@app.get("/searchNickName")
+async def search_nick_name(nickName: str):
     fifa4_nick_name_api_url="https://api.nexon.co.kr/fifaonline4/v1.0/users?nickname={}"
     fifa4_maxdivision_api_url="https://api.nexon.co.kr/fifaonline4/v1.0/users/{}/maxdivision"
     result={"accessId": "", "nickname":"","level":"","rank":"","autoRank":""}
+    print(nickName)
     try:
-        nick_name_response = requests.get(fifa4_nick_name_api_url.format(data.nickName), headers=header)
+        nick_name_response = requests.get(fifa4_nick_name_api_url.format(nickName), headers=header)
         if nick_name_response.status_code == 200:
-            """
-            accessId	String	유저 고유 식별자 
-            nickname	String	유저 닉네임 
-            level	Integer	유저 레벨 
-            """
+   
+            #accessId	String	유저 고유 식별자 
+            #nickname	String	유저 닉네임 
+            #level	Integer	유저 레벨 
+     
             nick_name_jsonStr = nick_name_response.json()
             accessId = nick_name_jsonStr['accessId']
             nickname = nick_name_jsonStr['nickname']
@@ -84,8 +176,6 @@ async def search_nick_name(data: SearchUserData):
                         maxdivision_info['matchType'] = '감독모드'
                         result["autoRank"] = maxdivision_info
                 print(result)
-                #result = json.dumps(result,ensure_ascii = False)
-                return result
             else:
                 raise HTTPException(status_code=maxdivision_response.status_code, detail=maxdivision_response.text)
         else:
@@ -93,3 +183,5 @@ async def search_nick_name(data: SearchUserData):
     except requests.RequestException as error:
         raise HTTPException(status_code=500, detail=str(error))
 
+    return result
+"""
